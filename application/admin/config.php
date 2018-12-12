a7017a346f99a662ee5e8d511028e0f74b241abf
linxiaocong <735207034@qq.com>
2018-12-12 19:19:42 +0800

    update

--- application/admin/config.php
+++ application/admin/config.php
@@ -1,9 +1,16 @@
 <?php
 //配置文件
 return [
-    'template'               => [
+    'template' => [
             'layout_on'     =>  true,
             'layout_name'   =>  'layout/layout',
         ],
+    'oss' => [
+        'accessId' => 'LTAIND66s1Od0JAZ',
+        'accessSecret' => '7TsnKr9BVK7yysNpoj75DnW7prJdZZ',
+        'endpoint' => 'oss-cn-hongkong.aliyuncs.com',
+        'bucket' => 'blog1688',
+        'domain' => 'http://image.blog1688.com',
+    ],
 //'show_error_msg' => true,//显示错误信息
 ];
--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -8,6 +8,10 @@ use think\Request;
 class Article extends Controller
 {
     public $status = [0 => '未发布', 1 => '已发布']; 
+    
+    public $column = [0 => '所有栏目显示',1 => '新闻资讯',2 => '├行业动态'];
+    
+    public $type = [0 => '全部类型', 1 => '个人博客文章', 2 => '帮助说明', 3 => '新闻资讯'];
 
 
     public function index()
@@ -15,19 +19,41 @@ class Article extends Controller
         if(Request::instance()->isPost()){
             $page = Request::instance()->post('page',1);
             $pageSize = Request::instance()->post('pageSize',20);
+            $status = Request::instance()->post('status');
+            $start_time = Request::instance()->post('start_time');
+            $end_time = Request::instance()->post('end_time');
+            $title = Request::instance()->post('title');
+            
+            $where = [];
+            if(isset($status) && $status != ''){
+                $where['status'] = $status;
+            }
+            if($start_time){
+                $start_time = strtotime($start_time . ' 00:00:00');
+                $where['addtime'] = ['>=',$start_time];
+            }
+            if($end_time){
+                $end_time = strtotime($end_time . ' 00:00:00');
+                $where['addtime'] = ['<=',$end_time];
+            }
+            if($title){
+                $where['title'] = $title;
+            }
 
-            $count = Db::table('article')->count();
+            $count = Db::table('article')->where($where)->count();
             $pageCount = ceil($count / $pageSize);
             $offset = (($page - 1) * $pageSize);
-            $list = Db::table('article')->select();
+            $list = Db::table('article')->where($where)->limit($offset,$pageSize)->select();
             
             if($list){
                 foreach ($list as $key => $val){
                     $list[$key]['status'] = $this->status[$val['status']];
+                    $list[$key]['column'] = $this->column[$val['column']];
+                    $list[$key]['type'] = $this->type[$val['type']];
                     $list[$key]['addtime'] = date('Y-m-d H:i:s',$val['addtime']);
                 }
             }
-            
+//            echo '<pre />';print_r($list);exit;
             $data['list'] = $list;
             $data['count'] = $count;
             $data['pageCount'] = $pageCount;
@@ -36,7 +62,7 @@ class Article extends Controller
             $this->success('请求成功',null,$data);
         }
 
-        return view('index');
+        return view('index',['status' => $this->status]);
     }
     
     public function add()
